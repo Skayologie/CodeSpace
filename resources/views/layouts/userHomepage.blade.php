f5caa3e6068af7c7a7084b55bd0ad2c3512559b7
Jawad Boulmal <jawadboulmal@gmail.com>
2025-03-16 00:07:03 +0000

    Feat : modify and add props of auth for getting the auth variable

--- resources/views/layouts/userHomepage.blade.php
+++ resources/views/layouts/userHomepage.blade.php
@@ -1,6 +1,6 @@
 @props(["auth"])
 <x-guest-layout>
-    <x-header></x-header>
+    <x-header auth="{{$auth}}"></x-header>
     <x-usersidebar auth="{{$auth}}"></x-usersidebar>
     <!-- Main Content Area -->
     <div class="flex flex-col gap-5 p-6">
