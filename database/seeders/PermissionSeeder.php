12498f0f837ae998169dea5765586b310325d73c
Jawad Boulmal <jawadboulmal@gmail.com>
2025-03-16 14:25:36 +0000

    Feat : Add a real values to the Permission Seeder for all actors like admin and the user and the guest and the manager

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -15,10 +15,61 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         Permission::insert([
-            ["name"=>"admin"],
-            ["name"=>"user"],
-            ["name"=>"guest"],
-            ["name"=>"manager"]
+            ["name" => "post.create"],
+            ["name" => "post.edit"],
+            ["name" => "post.delete"],
+            ["name" => "post.view"],
+            ["name" => "post.manage"],
+
+            ["name" => "comment.create"],
+            ["name" => "comment.edit"],
+            ["name" => "comment.delete"],
+            ["name" => "comment.view"],
+            ["name" => "comment.manage"],
+
+            ["name" => "user.view"],
+            ["name" => "user.manage"], // For admins or manager
+            ["name" => "user.ban"],
+            ["name" => "user.unban"],
+            ["name" => "user.assign_roles"],
+
+            ["name" => "category.create"],
+            ["name" => "category.edit"],
+            ["name" => "category.delete"],
+            ["name" => "category.view"],
+
+            ["name" => "tag.create"],
+            ["name" => "tag.edit"],
+            ["name" => "tag.delete"],
+            ["name" => "tag.view"],
+
+            ["name" => "report.create"], // Users can report content
+            ["name" => "report.view"],   // manager/Admins can see reports
+            ["name" => "report.manage"], // manager/Admins can act on reports
+
+            ["name" => "admin.access"], // Grants access to admin panel
+            ["name" => "settings.manage"], // Modify platform settings
+
+            ["name" => "role.create"],
+            ["name" => "role.edit"],
+            ["name" => "role.delete"],
+            ["name" => "role.assign"],
+
+            ["name" => "notification.view"],
+            ["name" => "notification.manage"],
+
+            ["name" => "message.send"],
+            ["name" => "message.view"],
+            ["name" => "message.delete"],
+
+            ["name" => "like.post"],
+            ["name" => "like.comment"],
+            ["name" => "dislike.post"],
+            ["name" => "dislike.comment"],
+
+            ["name" => "moderation.warn"],
+            ["name" => "moderation.suspend"],
+            ["name" => "moderation.delete"],
         ]);
     }
 }
