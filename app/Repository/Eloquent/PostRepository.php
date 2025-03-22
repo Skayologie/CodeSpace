88e849c6d5089a7d33b168c90824b4812bd4e22f
Jawad Boulmal <jawadboulmal@gmail.com>
2025-03-22 13:52:47 +0000

    Feat : modif on the post repository and add the store post method for store the post on the database

--- app/Repository/Eloquent/PostRepository.php
+++ app/Repository/Eloquent/PostRepository.php
@@ -10,22 +10,42 @@ class PostRepository
 {
     public function Store_tags(Post $post , $tags){
         try {
+            $count = 0;
             foreach ($tags as $tag){
                 DB::table('post_tags')->insert([
                     'post_id' => $post->id,
                     'tag_id' => $tag,
                 ]);
+                $count++;
             }
-            return true;
-        }catch(\Exception $e){
-            return false;
-        }
-    }
-    public function StorePost($request){
-        try {
-            Post::create();
+            if (count($tags) === $count){
+                return true;
+            }else{
+                return false;
+            }
         }catch(\Exception $e){
             return $e->getMessage();
+        }
+    }
+    public function StorePost($data){
+        try {
+            $type = ($_GET["Type"] === "content-texte") ? "text" :
+                (($_GET["Type"] === "content-image") ? "image" :
+                    (($_GET["Type"] === "content-link") ? "lien" : "null"));
+            $post = Post::create([
+                "userId"=>session()->get("user")->id,
+                "title"=>$data["title"],
+                "content"=>$data["content"],
+                "categoryID"=>$data["category"],
+                "type"=>$type,
+                "views"=>0,
+                "down_votes"=>0,
+                "up_votes"=>0,
+                "reactions"=>0
+            ]);
+            return $post;
+        }catch(\Exception $e){
+            return false;
         }
     }
 }
