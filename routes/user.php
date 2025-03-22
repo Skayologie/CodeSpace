a41f6d6cf328fd7c04d6e68c0a87bef42d637495
Jawad Boulmal <jawadboulmal@gmail.com>
2025-03-22 14:01:17 +0000

    Feat : Create endpoint for search tag for use it in the creation of the post

--- routes/user.php
+++ routes/user.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,4 +11,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(["auth", "role:user"])->group(function () {
     Route::resource('/Post', PostController::class);
     Route::resource('/Profile', ProfileController::class);
+
+    Route::get("Tag/Search/{text}",[TagController::class,"tag_search"]);
 });
