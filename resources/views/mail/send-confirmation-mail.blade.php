7ea3f77186b441a49768d1ac238122af83f8c33c
Jawad Boulmal <jawadboulmal@gmail.com>
2025-04-19 14:53:01 +0100

    Feat : Add the template of the confirmation mail

---
+++ resources/views/mail/send-confirmation-mail.blade.php
@@ -0,0 +1,5 @@
+<div>
+    Confirm You Email
+    <a href="http://127.0.0.1:9999/verifyEmail/CheckToken/{{$token}}">Confirm Your Email By Clicking Here !</a>
+    This is you email confirmation
+</div>
