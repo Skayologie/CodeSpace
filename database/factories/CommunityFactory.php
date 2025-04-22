f656d633bac2c3f1c8b6aebf5dcef67273d4d693
Jawad Boulmal <jawadboulmal@gmail.com>
2025-04-23 00:37:54 +0100

    chore: Update CommunityFactory with new fields

--- database/factories/CommunityFactory.php
+++ database/factories/CommunityFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Community>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CommunityMembers>
  */
 class CommunityFactory extends Factory
 {
