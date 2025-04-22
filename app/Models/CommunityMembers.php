d54ba2d74248a2abf6eba38c9cc02822eed1e03d
Jawad Boulmal <jawadboulmal@gmail.com>
2025-04-23 00:37:54 +0100

    feat: Create CommunityMembers model and status column migration

---
+++ app/Models/CommunityMembers.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class CommunityMembers extends Pivot
+{
+    protected $table = "community_members";
+    protected $fillable = [
+        "communityID",
+        "userId",
+        "role",
+        "status"
+    ];
+    public function user(){
+        return $this->hasOne(User::class,"id","userId");
+    }
+}
---
+++ database/migrations/2025_04_20_232201_create_status_columns.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('community_members', function (Blueprint $table) {
+            $table->enum("status",["null","invited","regularUser"])->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('communities', function (Blueprint $table) {
+            $table->dropColumn(['status']);
+        });
+
+    }
+};
