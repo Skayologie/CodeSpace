29487c1c3d4f27a4fd3e7cb7d20a0ada77e007d7
Jawad Boulmal <jawadboulmal@gmail.com>
2025-04-24 21:35:28 +0100

    fix: Update user routes to reflect new features

--- routes/user.php
+++ routes/user.php
@@ -16,17 +16,21 @@ Route::middleware(["auth", "role:user"])->group(function () {
     Route::resource('/Post', PostController::class);
     Route::resource('/Profile', ProfileController::class);
     Route::resource('/Community', CommunityController::class);
+    Route::get('/Explore/Community/{id}', [CommunityController::class, 'show'])->name("Explore.community");
+    Route::resource('/Explore',ExploreController::class);
     Route::get('/AllThemes', [ThemeController::class,"getAllThemes"]);
-    Route::get('/Explore/Communities',[ExploreController::class,"getCommunities"]);
+//    Route::get('/Explore/Communities',[ExploreController::class,"getCommunities"]);
     Route::get("Tag/Search/{text}",[TagController::class,"tag_search"]);
 
-    Route::get("Community/join/{communityID}/{userID}",[CommunityController::class,"rejoindre"]);
+    Route::get("Community/join/{communityID}",[CommunityController::class,"rejoindre"]);
+    Route::get("Communities/listAllMyCommunities",[CommunityController::class,"listCommunityDispo"]);
 
     Route::get("ManageCommunity/inviteModo",[CommunityController::class,"inviteForm"]);
     Route::get("ManageCommunity/SendInviteToCommunity/{CommunityID}/{UserID}/{role}",[CommunityController::class,"SendInvite"]);
     Route::get("ManageCommunity/CheckInvitation/{CommunityID}/{UserID}/{role}",[CommunityController::class,"CheckInvite"]);
 
     Route::get("Users/Search/{nameQuery}",[UserController::class,"SearchUsers"]);
+    Route::get("Communities/Search/{nameQuery}",[CommunityController::class,"SearchCommunity"]);
 
 
 });
