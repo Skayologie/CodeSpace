a31b1ad3f1f4b20cd9e70dcdcf7ac931700972fa
Jawad Boulmal <jawadboulmal@gmail.com>
2025-04-24 21:35:21 +0100

    fix: Refactor ExploreController logic

--- app/Http/Controllers/ExploreController.php
+++ app/Http/Controllers/ExploreController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Community;
+use App\Models\CommunityMembers;
 use App\Models\Theme;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
@@ -10,6 +11,12 @@ use Illuminate\Support\Facades\File;
 class ExploreController extends Controller
 {
     //
+    public function index(){
+        return view("Explore.index",[
+            "Themes"=>Theme::where("parent","=",null)->paginate(6),
+            "Communities"=>Community::withCount('members')->orderByDesc('members_count')->get(),
+        ]);
+    }
     public function getCommunities(){
         return view("User.Pages.Explore",[
             "Themes"=>Theme::where("parent","=",null)->paginate(6),
