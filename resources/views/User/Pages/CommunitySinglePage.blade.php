1d18fe3e406e5ef5f0bc44d3a6badab9ba8a14e1
Jawad Boulmal <jawadboulmal@gmail.com>
2025-04-24 21:35:25 +0100

    feat: Refactor CommunitySinglePage view

--- resources/views/User/Pages/CommunitySinglePage.blade.php
+++ resources/views/User/Pages/CommunitySinglePage.blade.php
@@ -1,3 +1,4 @@
+<x-UserHompage>
 <div class="container mx-auto p-0 max-w-6xl">
 
     <!-- Header Banner -->
@@ -315,3 +316,80 @@
     </div>
 </div>
 
+    <div>
+        <div id="inviteModos" class="hidden fixed inset-0 bg-black/50 bg-opacity-50 flex items-center justify-center z-[120]">
+            <!-- Modal Container -->
+            <div class="rounded-lg shadow-xl w-[700px] overflow-hidden bg-white ">
+                <!-- Modal Header -->
+                <div class="flex justify-between items-center p-4 ">
+                    <h2 class="text-xl font-medium text-gray-800">Inviter un·e modo</h2>
+                    <button id="closeBtn" class="text-gray-500 hover:text-gray-700">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                        </svg>
+                    </button>
+                </div>
+
+                <!-- Modal Body Name And description -->
+                <div id="1" class="p-4">
+                    <!-- Search Bar -->
+                    <div class="relative mb-4">
+                        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                            <svg class="h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                            </svg>
+                        </div>
+                        <input id="SearchUserInput" type="text" class="pl-10 w-full bg-gray-100 border-none rounded-lg py-2 px-4 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Search users to invite">
+                    </div>
+
+
+                    <!-- Theme Categories -->
+                    <div id="UsersInvite" class="space-y-6 max-h-96 overflow-y-auto pr-2">
+
+                    </div>
+                    <div id="default-modal" tabindex="-1" aria-hidden="true" class="flex hidden justify-self-center overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                        <div class="relative p-4 w-full max-w-2xl max-h-full">
+                            <!-- Modal content -->
+                            <div class="relative bg-white rounded-lg shadow-sm ">
+                                <!-- Modal header -->
+                                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t  border-gray-200">
+                                    <h3 class="text-xl font-semibold text-gray-900 ">
+                                        Accept or Decline
+                                    </h3>
+                                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center " data-modal-hide="default-modal">
+                                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                        </svg>
+                                        <span class="sr-only">Close modal</span>
+                                    </button>
+                                </div>
+                                <!-- Modal body -->
+                                <div class="p-4 md:p-5 space-y-4">
+                                    <p id="messageSentInvite" class="text-base leading-relaxed text-gray-500 ">
+
+                                    </p>
+                                </div>
+                                <!-- Modal footer -->
+                                <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b">
+                                    <button id="AcceptBtn" data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                                    <button id="DeclineBtn" data-modal-hide="default-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Modal Footer -->
+                <div class="bg-gray-50 px-4 py-3 flex justify-between space-x-2">
+                    <div>
+                        <button data-progress="0" id="cancelBtn" class="px-4 py-2 bg-gray-100 text-gray-900 font-medium rounded-full hover:bg-gray-200">
+                            Annulez
+                        </button>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</x-UserHompage>
