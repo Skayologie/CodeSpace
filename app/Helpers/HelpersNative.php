a384577ee7e8f4273777367d7b8fcff2bc771b1f
Jawad Boulmal <jawadboulmal@gmail.com>
2025-04-25 12:44:30 +0100

    Feat : Add HelperNative for add functions for help the usage of the other methods

---
+++ app/Helpers/HelpersNative.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Helpers;
+class HelpersNative
+{
+    public static function formatNumberShort($number): string {
+        if ($number >= 1000000000) {
+            return round($number / 1000000000, 1) . 'B';
+        } elseif ($number >= 1000000) {
+            return round($number / 1000000, 1) . 'M';
+        } elseif ($number >= 1000) {
+            return round($number / 1000, 1) . 'K';
+        }
+
+        return (string) $number;
+    }
+}
