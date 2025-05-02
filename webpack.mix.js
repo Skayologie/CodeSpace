ca988b5556661a2352d8ecf544b849914ee6eb11
Jawad Boulmal <jawadboulmal@gmail.com>
2025-05-02 18:07:00 +0100

    Remove the bootstrap folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 
-mix.js("resources/js/main.js", "public/js").js("resources/js/bootstrap/bootstrap.js", "public/bootstrap")
+mix.js("resources/js/main.js", "public/js")
     .postCss("resources/css/app.css", "public/css", [
         require("tailwindcss"),
     ]);
