ec44ffd1d247eb84d39c1383107bd2719c9b3954
Jawad Boulmal <jawadboulmal@gmail.com>
2025-05-05 16:18:07 +0100

    Initialize Comments.js in main script

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,6 +10,7 @@ import {
     ChangeConversation
 } from "./main/ui";
 import Settings from "./main/Settings"
+import {SendComment,DeleteMyComment} from "./main/Comments"
 function main() {
     try { InviteModo(); } catch (e) { console.error(e); }
     try { JoinCommunity(); } catch (e) { console.error(e); }
@@ -21,6 +22,8 @@ function main() {
     try { ChangeConversation(); } catch (e) { console.error(e); }
     try { sentMessage(); } catch (e) { console.error(e); }
     try { Settings(); } catch (e) { console.error(e); }
+    try { SendComment(); } catch (e) { console.error(e); }
+    try { DeleteMyComment(); } catch (e) { console.error(e); }
 }
 document.addEventListener('DOMContentLoaded', main);
 
